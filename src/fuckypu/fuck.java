928edebb138389837bd80ab9802ac7b73dd0b0f6
camilo <camilo171512@gmail.com>
2025-09-24 20:13:01 -0500

    feat: :zap: hice la clase person

---
+++ src/fuckypu/fuck.java
@@ -0,0 +1,27 @@
+package fuckypu;
+public class fuck {
+private int id;
+private int docNum;
+public fuck(int id, int docNum) {
+    this.id = id;
+    this.docNum = docNum;
+}
+public int getId() {
+    return id;
+}
+public void setId(int id) {
+    this.id = id;
+}
+public int getDocNum() {
+    return docNum;
+}
+public void setDocNum(int docNum) {
+    this.docNum = docNum;
+}
+@Override
+public boolean equals(Object obj) {
+    // TODO Auto-generated method stub
+    return super.equals(obj);
+}
+
+}
